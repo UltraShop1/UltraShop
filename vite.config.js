2f6258fe84e5967bc33933a08aefc4a1037d034e
UltraShop1 <nassymferg@gmail.com>
2025-04-11 19:06:33 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Assurez-vous que le dossier de sortie est bien "dist"
+  }
+});
